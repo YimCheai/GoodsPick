c3436062b7cc8574ca1a0fe6337d2ea285176dac
YimCheai <babybreadin1025@gmail.com>
2025-08-30 20:14:02 +0900

    test commit

---
+++ src/main/java/com/goodspick/goodspick/GoodsPickApplication.java
@@ -0,0 +1,13 @@
+package com.goodspick.goodspick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsPickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsPickApplication.class, args);
+	}
+
+}
