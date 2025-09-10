5664383487f847d24210da51ee0b7b0b0c8ae028
조현서 <hyeonseojo929@gmail.com>
2025-09-10 11:54:40 +0900

    Initial commit

---
+++ goodspick/src/main/java/com/example/demo/GoodspickApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodspickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodspickApplication.class, args);
+	}
+
+}
---
+++ goodspick/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>테스트 페이지</title>
+</head>
+<body>
+    <h1>Hello, JSP!</h1>
+</body>
+</html>
